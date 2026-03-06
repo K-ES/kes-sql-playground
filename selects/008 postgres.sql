b323ea2e4d2258aa359bc13ceee39725d1bc5b2c
K-ES <your@email>
2026-03-06 15:57:40 +0200

    скрипты для тестовой базы данных

--- selects/008 postgres.sql
+++ selects/008 postgres.sql
@@ -1,18 +1,1 @@
--- 8 Найдите производителя, выпускающего ПК, но не ПК-блокноты.
-
--- некорректно, сглючит на NULL
-select distinct maker
-from Product
-where type = 'PC'
-  and maker not in (
-      select maker from Product where type = 'Laptop'
-  );
-
-
-select maker
-from Product
-where type = 'PC'
-except
-select maker
-from Product
-where type = 'Laptop';
+select * from product
---
+++ sql_training/01 создание базы данных.sql
@@ -0,0 +1,24 @@
+--SQL TRAINING DATABASE
+
+--1. СОЗДАНИЕ БАЗЫ
+
+--Подключиться к базе postgres и выполнить:
+
+CREATE DATABASE sql_training;
+
+
+--2. ПОДКЛЮЧЕНИЕ
+
+--В DBeaver:
+--Database → sql_training → Open SQL Editor
+
+
+--3. СОЗДАНИЕ СХЕМЫ
+
+CREATE SCHEMA training;
+
+
+--4. ДАЛЬНЕЙШАЯ РАБОТА
+--
+--После создания схемы все учебные таблицы и задания
+--создаются внутри схемы training.
