b8ecd48d6b42d727e4b8bc87498f41b5e7b674b0
K-ES <your@email>
2026-03-12 03:56:12 +0200

    отправка нового файла

--- DBeaverSettings/workplace/General/Scripts/018 самые дешевые цветные принтеры.sql
+++ DBeaverSettings/workplace/General/Scripts/018 самые дешевые цветные принтеры.sql
@@ -1,1 +1,8 @@
- select * from printer
+--Найдите производителей самых дешевых цветных принтеров. Вывести: maker, price
+
+-- просто в лоб
+select distinct pro.maker , t.price  from printer t
+join product pro on pro.model = t.model 
+where t.color  = 'y'
+and t.price = (select min(price) from printer p2 where p2.color  = 'y')
+ 
