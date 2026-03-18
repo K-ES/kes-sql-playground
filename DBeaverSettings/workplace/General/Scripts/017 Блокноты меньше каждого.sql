d8267cd98cdd2900fa5730b814d1bbd1b16008c0
K-ES <your@email>
2026-03-18 07:32:37 +0200

    017 end

---
+++ DBeaverSettings/workplace/General/Scripts/017 Блокноты меньше каждого.sql
@@ -0,0 +1,7 @@
+-- 017 Найдите модели ПК-блокнотов, скорость которых меньше скорости каждого из ПК.
+
+-- Опять не ясно, зачем distinct
+select distinct p."type", t.model, speed from laptop t
+join product p on p.model  = t.model 
+where speed < (select min(speed) from pc)
+
